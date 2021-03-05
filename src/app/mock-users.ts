3d7cbe86d515971465fa195d00f429b173cb2084
Rafia Hossain <rrhossain101@gmail.com>
2021-03-05 13:30:31 -0500

    initial commit

---
+++ src/app/mock-users.ts
@@ -0,0 +1,14 @@
+import { User } from './user';
+
+export const USERS: User[] = [
+  { id: 1001, name: 'Eunbi' },
+  { id: 1002, name: 'Tom' },
+  { id: 1003, name: 'Tomoe' },
+  { id: 1004, name: 'Celeste' },
+  { id: 1005, name: 'Phil' },
+  { id: 1006, name: 'Cas' },
+  { id: 1007, name: 'Noel' },
+  { id: 1008, name: 'Elizabeth' },
+  { id: 1009, name: 'Rob' },
+  { id: 2000, name: 'John' }
+];
